4c30360e4a00fb2ad2642dd2d49d71418d5efc44
Jenkins <jenkins@wens-mbp.attlocal.net>
2020-12-14 11:37:41 -0500

    initial reactive form -- wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputformsComponent } from './inputforms/inputforms.component';
@@ -12,7 +13,9 @@ import { InputformsComponent } from './inputforms/inputforms.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
